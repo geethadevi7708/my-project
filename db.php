2fb1d746ede47cfcd523737cbe908f06dbac1ed5
geethadevi7708 <geethadevi7708@gmail.com>
2025-04-14 20:21:51 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,8 @@
 <?php
-$conn = mysqli_connect("localhost",'root','','finalprojectuser')
+$conn = mysqli_connect("localhost",'root','','finalprojectuser');
+if($conn){
+  echo "connected";
+}else{
+  echo "not connected";
+}
 ?>
